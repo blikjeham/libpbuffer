d9cc48e0a977cc055dc33ee6d127622dd62d18db
Wybe van der Ham <wybe@snth.eu>
2011-10-08 10:48:40 +0200

    Added some comments to the header file.
    This should make it a bit more easy to use the library.
    Also removed the PBUFMIN define. We don't use it. The first alloc
    is of 0 bytes.

--- pbuffer.h
+++ pbuffer.h
@@ -16,17 +16,19 @@ struct pbuffer {
 	char *data;
 };
 
-/* the minimum size of a pbuffer */
-#define PBUFMIN 32
-
+/* Allocate memory for the buffer. Return the pointer to the buffer. */
 pbuffer *pbuffer_new(void);
 
+/* Set the buffer to this value. */
 void pbuffer_set(pbuffer *, char *);
 
+/* Add this string to the buffer. */
 void pbuffer_add(pbuffer *, char *);
 
+/* Grow the buffer to this new size */
 ssize_t pbuffer_grow(pbuffer *, ssize_t );
 
+/* Free the buffer data and the buffer itself. */
 void pbuffer_free(pbuffer *);
 
 #endif /* HAVE_PBUFFER_H */
